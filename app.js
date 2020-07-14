870da09fcd93165c2cc964398241a2c85bff8cb5
jprevatte <37543780+jprevatte@users.noreply.github.com>
2020-07-14 17:13:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ function leaderComms(thisURL) {
             ,"d75306e4-7831-4a23-9f5b-65eda43fcda1":"chapt/tt"
             ,"bf526743-1f97-4ac9-8a8d-0fd9546184d6":"chapt/ci"
             ,"5264daf8-fb05-4143-b873-86889f166b8f":"chapt/wmpf"
+            ,"fbcea787-9670-4abd-9f37-4f563dd8c2f1":"chapt/irsft"
         }
         function getUrlVars() {
             var vars = {};
